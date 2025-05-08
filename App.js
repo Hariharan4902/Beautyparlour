b9d008cd7fe15f8948a05af377388848c4743841
Hariharan4902 <hhari2804@gmail.com>
2025-05-08 12:16:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
